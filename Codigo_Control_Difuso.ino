57f738a3f8af5caefa01341be32188e3a8923921
BautistaLRafa <rafaelbautistalopez@gmail.com>
2015-10-06 00:22:06 -0500

    Asignación de valores a los conjuntos de entrada (Entrada_Error)
    1. Paso de 4 a 5 conjuntos de Entrada_Error (float F[5];).
    2. Determinar valor de referencia.
    3. Determinar valores de Ea, Eb, Ec y Ed correspondientes para cada
    condicional.
    4. Incluir puntos de Ena, Enb, Enc y End para que la función aplique a
    un conjunto de forma triangular.

--- Codigo_Control_Difuso.ino
+++ Codigo_Control_Difuso.ino
@@ -1,5 +1,5 @@
 #include <TimerOne.h>
-float Ea, Eb, Ec, Ed, En, vol, F[4], Error;
+float Ea, Eb, Ec, Ed, En, vol, F[5], Error;
 int sensor;
 
 void setup(void)
@@ -7,7 +7,7 @@ void setup(void)
     pinMode(13,OUTPUT);
     Timer1.initialize(10000);                 // Dispara cada 10 ms
     Timer1.attachInterrupt(ISR_Interrupcion); // Activa la interrupcion y la asocia a ISR_Interrupcion
-    Serial.begin(9600);
+    Serial.begin(38400);
    }
 void loop() {
   
@@ -17,44 +17,51 @@ void ISR_Interrupcion(){
  sensor = analogRead(A0);
  vol = sensor * (5.0 / 1023.0);
  Serial.println(vol);
- float ref= ; // falta determinar
+ float ref= 2.63;
  Error=ref-vol;
-  if(Error<6 && Error>2){ 
-     Ea=2; Eb=3.5; Ec=4.5; Ed=6;
+  if(Error<4.7 && Error>2){ 
+     Ea=2; Eb=3.5; Ec=4; Ed=4.7;
      F[0] = Fuzzy(Error, Ea, Eb, Ec, Ed);}
      else{
      F[0]=0;
      }
-  if(Error<3 && Error>-1){
-     Ea=-1; Eb=0.5; Ec=1.5; Ed=3;
+  if(Error<3 && Error>0){
+     Ea=0; Eb=1; Ec=2; Ed=3;
      F[1] = Fuzzy(Error, Ea, Eb, Ec, Ed);}
      else{
      F[1]=0;
      }
-  if(Error<0 && Error>-4){
-     Ea=-4; Eb=-3; Ec=-1; Ed=0;
+  if(Error<0.5 && Error>-0.5){
+     Ea=-0.5; Eb=0; Ec=0; Ed=0.5;
      F[2] = Fuzzy(Error, Ea, Eb, Ec, Ed);}
      else{
      F[2]=0;
      }
-  if(Error<-3 && Error>-6){
-     Ea=-6; Eb=-5; Ec=-4; Ed=-3;
+  if(Error<0 && Error>-3){
+     Ea=-3; Eb=-2; Ec=-1; Ed=0;
      F[3] = Fuzzy(Error, Ea, Eb, Ec, Ed);}
      else{
      F[3]=0;
+     }
+  if(Error<-2 && Error>-4.7){
+     Ea=-4.7; Eb=-4; Ec=-3.5; Ed=-2;
+     F[4] = Fuzzy(Error, Ea, Eb, Ec, Ed);}
+     else{
+     F[4]=0;
      }
 }
 //----------------Fuzzificacion--------------------
 float Fuzzy(float E, float Ena, float Enb, float Enc, float End)
 {
   float P;
-  if(E<Ena || E>End)
+  if(E<=Ena || E>=End)
     { P=0;}
     else
-  if(E>Enb && E<Enc)
+  if(E>=Enb && E<=Enc)
     { P=1;}
     else
-  if(E<=Enb || E>=Enc)
+  if(E<Enb || E>Enc)
     {P=(E-Ena)/(Enb-Ena);}
   return P;
 }
+//------------------------------------------------------
