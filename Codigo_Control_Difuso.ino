d0f95ecb81695c5872feac665624310b16a08819
BautistaLRafa <rafaelbautistalopez@gmail.com>
2015-10-16 01:16:19 -0500

    Arreglo Implicación conjuntos salida

--- Codigo_Control_Difuso.ino
+++ Codigo_Control_Difuso.ino
@@ -31,11 +31,8 @@ void ISR_Interrupcion(){
      if(Error>=Enb && Error<=Enc)
      { Emp=1;}
      else
-     if(Error<Enb || Error>Ena)
+     if(Error<Enb || Error>Enc)
      {Emp=(Error-Ena)/(Enb-Ena);}
-     else
-     if(Error<End || Error>Enc)
-     {Emp=(Error+Ena)/(End-Enc);}
      else{
      Emp=0;}
      }
@@ -47,11 +44,8 @@ void ISR_Interrupcion(){
      if(Error>=Enb && Error<=Enc)
      { Ep=1;}
      else
-     if(Error<Enb || Error>Ena)
+     if(Error<Enb || Error>Enc)
      {Ep=(Error-Ena)/(Enb-Ena);}
-     else
-     if(Error<End || Error>Enc)
-     {Ep=(Error+Ena)/(End-Enc);}
      else{
      Ep=0;}
      }
@@ -63,11 +57,8 @@ void ISR_Interrupcion(){
      if(Error>=Enb && Error<=Enc)
      { Ez=1;}
      else
-     if(Error<Enb || Error>Ena)
+     if(Error<Enb || Error>Enc)
      {Ez=(Error-Ena)/(Enb-Ena);}
-     else
-     if(Error<End || Error>Enc)
-     {Ez=(Error+Ena)/(End-Enc);}
      else{
      Ez=0;}
      }
@@ -79,11 +70,8 @@ void ISR_Interrupcion(){
      if(Error>=Enb && Error<=Enc)
      { En=1;}
      else
-     if(Error<Enb || Error>Ena)
+     if(Error<Enb || Error>Enc)
      {En=(Error-Ena)/(Enb-Ena);}
-     else
-     if(Error<End || Error>Enc)
-     {En=(Error+Ena)/(End-Enc);}
      else{
      En=0;}
      }
@@ -95,11 +83,8 @@ void ISR_Interrupcion(){
      if(Error>=Enb && Error<=Enc)
      { Emn=1;}
      else
-     if(Error<Enb || Error>Ena)
+     if(Error<Enb || Error>Enc)
      {Emn=(Error-Ena)/(Enb-Ena);}
-     else
-     if(Error<End || Error>Enc)
-     {Emn=(Error+Ena)/(End-Enc);}
      else{
      Emn=0;}
      }
@@ -112,11 +97,8 @@ if(Eprima<=4.7 && Eprima>=2){           // Falta definir conjuntos para E'(Error
      if(Eprima>=Enb && Eprima<=Enc)
      { Epmp=1;}
      else
-     if(Eprima<Enb || Eprima>Ena)
+     if(Eprima<Enb || Eprima>Enc)
      {Epmp=(Eprima-Ena)/(Enb-Ena);}
-     else
-     if(Error<End || Error>Enc)
-     {Epmp=(Error+Ena)/(End-Enc);}
      else{
      Epmp=0;}
      }
@@ -223,95 +205,102 @@ for (int i=0; i<5; i++){
         MaxOmp=Omp[i];}
 }
 
-float SumP=0, Sum=0, Omp, Op, Oz, On, Omn;
+float SumP=0, Sum=0, Omp1, Op1, Oz1, On1, Omn1;
 for (int i=0; i<101; 101/1023){
     if(i<=1481 && i>=932){           
      Ena=932; Enb=1176; Enc=1237; End=1481;
      if(i<=Ena || i>=End)
-     { Omp=0;}
+     { Omp1=0;}
      else
      if(i>=Enb && i<=Enc)
-     { Omp=MaxOmp;}
+     { Omp1=MaxOmp;}
      else
-<<<<<<< HEAD
      if(i<Enb || i>Ena)
-     {Salida=(i-Ena)/(Enb-Ena);}
+     {Omp1=(i-Ena)/(Enb-Ena);}
      else
-     if(Error<End || Error>Enc)
-     {Salida=(Error+Ena)/(End-Enc);}
-=======
-     if(i<Enb || i>Enc)
-     {Omp=(i-Ena)/(Enb-Ena);}
->>>>>>> origin/master
+     if(i<End || i>Enc)
+     {Omp1=(i+Ena)/(End-Enc);}
      else{
      Omp=0;}
      }
-    sum=sum+Omp;
-    sump=sump+(Omp*i);
+    sum=sum+Omp1;
+    sump=sump+(Omp1*i);
     
      if(i<=1110 && i>=623){           
      Ena=623; Enb=888; Enc=888; End=1110;
      if(i<=Ena || i>=End)
-     { Op=0;}
+     { Op1=0;}
      else
      if(i>=Enb && i<=Enc)
-     { Op=MaxOp;}
+     { Op1=MaxOp;}
      else
      if(i<Enb || i>Enc)
-     {Op=(i-Ena)/(Enb-Ena);}
+     {Op1=(i-Ena)/(Enb-Ena);}
+     else
+     if(i<End || i>Enc)
+     {Op1=(i+Ena)/(End-Enc);}
      else{
-     Op=0;}
+     Op1=0;}
      }
-    sum=sum+Op;
-    sump=sump+(Op*i);
+    sum=sum+Op1;
+    sump=sump+(Op1*i);
      
      if(i<=830 && i>=278){           
      Ena=278; Enb=578; Enc=578; End=830;
      if(i<=Ena || i>=End)
-     { Oz=0;}
+     { Oz1=0;}
      else
      if(i>=Enb && i<=Enc)
-     { Oz=MaxOz;}
+     { Oz1=MaxOz;}
      else
-     if(i<Enb || i>Enc)
-     {Oz=(i-Ena)/(Enb-Ena);}
+     if(i<Enb || i>Ena)
+     {Oz1=(i-Ena)/(Enb-Ena);}
+     else
+     if(i<End || i>Enc)
+     {Oz1=(i+Ena)/(End-Enc);}
      else{
-     Oz=0;}
+     Oz1=0;}
      }
-    sum=sum+Oz;
-    sump=sump+(Oz*i);
+    sum=sum+Oz1;
+    sump=sump+(Oz1*i);
      
      if(i<=478 && i>=16){           
      Ena=16; Enb=275; Enc=275; End=478;
      if(i<=Ena || i>=End)
-     { On=0;}
+     { On1=0;}
      else
      if(i>=Enb && i<=Enc)
-     { On=MaxOn;}
+     { On1=MaxOn;}
      else
-     if(i<Enb || i>Enc)
+     if(i<Enb || i>Ena)
      {On=(i-Ena)/(Enb-Ena);}
+     else
+     if(i<End || i>Enc)
+     {On1=(i+Ena)/(End-Enc);}
      else{
-     On=0;}
+     On1=0;}
      }
-    sum=sum+On;
-    sump=sump+(On*i);
+    sum=sum+On1;
+    sump=sump+(On1*i);
      
      if(i<=187 && i>=-291){           
      Ena=-291; Enb=0; Enc=45; End=187;
      if(i<=Ena || i>=End)
-     { Omn=0;}
+     { Omn1=0;}
      else
-     if(i>=Enb && i<=Enc)
-     { Omn=MaxOmn;}
+     if(i>=Enb && i<=Ena)
+     { Omn1=MaxOmn;}
      else
      if(i<Enb || i>Enc)
-     {Omn=(i-Ena)/(Enb-Ena);}
+     {Omn1=(i-Ena)/(Enb-Ena);}
+     else
+     if(i<End || i>Enc)
+     {Omn1=(i+Ena)/(End-Enc);}
      else{
-     Omn=0;}
+     Omn1=0;}
      }
-    sum=sum+Omn;
-    sump=sump+(Omn*i);
+    sum=sum+Omn1;
+    sump=sump+(Omn1*i);
  }
 }
 //----------------Fuzzificacion--------------------
